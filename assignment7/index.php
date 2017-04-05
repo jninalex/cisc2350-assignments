1c43b33d425beb57bc2f1b0be9e2302540229660
Jeanne Nina Laurel <jlaurel@fordham.edu>
2017-04-05 18:44:09 -0400

    updating assignment 7

--- assignment7/index.php
+++ assignment7/index.php
@@ -50,6 +50,7 @@
 		echo 'So far, I have taken ' . $classes["CISC1400"] . ', ' . $classes["CISC1600"] . ', ' . $classes["CISC2000"] . ', ' . $classes["CISC2540"] . ', ' . $classes["CISC3593"] . ', ' . $classes["MATH1206"] . ', ' . $classes["MATH1207"] . ', ' . $classes["NSCI1321"] . ', and ' . $classes["NSCI1501"] . ' to fulfill my Computer Science major.';
 		?>
 
+
 		<!-- using arithmetic operators-->
 		<h3>Credits:</h3>
 		<?php
@@ -57,7 +58,45 @@
 		$earned_credits = 95;
 		$total_credits = 124;
 		$credits_needed = $total_credits - $earned_credits;
+
 		echo 'I currently have ' . $earned_credits . ' credits, and the minimum amount needed to graduate is ' . $total_credits . ' credits. Thus, I still need to earn ' . $credits_needed . ' credits in order to graduate.';
+		?>
+
+
+		<!-- using conditional statements -->
+		<h3>Class:</h3>
+		<?php
+
+		$earned_credits = 95;
+		$total_credits = 124;
+		$credits_needed = $total_credits - $earned_credits;
+
+		//calculate needed credits
+		$to_be_sophomore = 30 - $earned_credits;
+		$to_be_junior = 60 - $earned_credits;
+		$to_be_senior = 92 - $earned_credits;
+
+		//check classification
+		if ($earned_credits < 30) {
+			echo "I am classified as a Freshman.";
+			echo '<br>';
+			echo 'I need ' . $to_be_sophomore . ' credits to become a Sophomore.';
+		} elseif ($earned_credits < 60) {
+			echo "I am classified as a Sophomore.";
+			echo '<br>';
+			echo 'I need ' . $to_be_junior . ' credits to become a Junior.';
+
+		} elseif ($earned_credits < 92) {
+			echo "I am classified as a Junior.";
+			echo '<br>';
+			echo 'I need ' . $to_be_senior . ' credits to become a Senior.';
+
+		} elseif ($earned_credits >= 92) {
+			echo "I am classified as a Senior.";
+			echo '<br>';
+			echo 'I need at least ' . $credits_needed . ' credits more to graduate.';
+		}
+
 		?>
 
 	</body>
