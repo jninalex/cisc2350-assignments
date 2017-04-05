8ee373e418ad03673e452745ee6a6cfa898a8c20
Jeanne Nina Laurel <jlaurel@fordham.edu>
2017-04-05 18:20:05 -0400

    updating assignment 7

--- assignment7/index.php
+++ assignment7/index.php
@@ -20,11 +20,11 @@
 		<h1>Hello!</h1>
 		<h2>My name is <?php echo MY_NAME; ?></h2>
 
-		<p>My birthday is on <?php echo $birthday; ?></p>
+		<p>My birthday is on <?php echo $birthday; ?>. </p>
 
 
 		<!-- using indexed arrays -->
-		<h3>My Favorite Food:</h3>
+		<h3>My Favorite Foods:</h3>
 
 		<?php 
 		//indexed array of favorite food
@@ -47,7 +47,20 @@
 		$classes = array("CISC1400" => "Discrete Structures", "CISC1600" => "Computer Science I", "CISC2000" => "Computer Science II", "CISC 2200" => "Data Structures", "CISC2540" => "Intro to Video Game Design", "CISC3593" => "Computer Organization", "MATH1206" => "Calculus I", "MATH1207" => "Calculus II", "NSCI1321" => "General Chemistry I", "NSCI1501" => "General Physics I");
 
 		//list array elements
-		echo 'So far, I have taken ' . $classes['CISC1400'] . ', ' . $classes['CISC1600'] . ', '$classes['CISC2000'] . ', ' . $classes["CISC2540"] . ', ' . $classes["CISC3593"] . ', ' . $classes["MATH1206"] . ', ' . $classes["MATH1207"] . ', ' . $classes["NSCI1321"] . ', and ' . $classes["NSCI1501"] . ' to fulfull my Computer Science major.';
+		echo 'So far, I have taken ' . $classes["CISC1400"] . ', ' . $classes["CISC1600"] . ', ' . $classes["CISC2000"] . ', ' . $classes["CISC2540"] . ', ' . $classes["CISC3593"] . ', ' . $classes["MATH1206"] . ', ' . $classes["MATH1207"] . ', ' . $classes["NSCI1321"] . ', and ' . $classes["NSCI1501"] . ' to fulfull my Computer Science major.';
+		?>
+
+		<!-- using arithmetic operators-->
+		<h3>Credits:</h3>
+		<?php
+
+		//variables
+		$earned_credits = 95;
+		$total_credits = 124;
+
+		$credits_needed = $total_credits - $earned_credits;
+
+		echo 'I currently have ' . $earned_credits . ' credits, and the minimum amount needed to graduate is ' . $total_credits . ' credits. Thus, I still need to earn ' . $credits_needed . ' credits in order to graduate.';
 		?>
 
 	</body>
